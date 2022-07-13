f029a77d7ad7d65690903eaa7d8a653988bda2ff
xuepengLiu <1946357291@qq.com>
2022-07-13 23:24:50 +0800

    第一次提交

---
+++ app/src/main/java/com/example/jisuanqi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.jisuanqi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nameTextView.text = "hello kitty"
+
+    }
+}
